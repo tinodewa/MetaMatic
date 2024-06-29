200e1bae112262aae7da663220b2f199b546165d
tinodewa <dtino0812@gmail.com>
2024-06-29 13:47:59 +0700

    add rotation feature to engine, transmission, and supercharger

--- Assets/Exhibition Scripts/RotateObjectScript.cs
+++ Assets/Exhibition Scripts/RotateObjectScript.cs
@@ -6,35 +6,29 @@ using SpatialSys.UnitySDK;
 public class RotateObjectScript : MonoBehaviour
 {
     public GameObject rotateObject;
-    //public SpatialTriggerEvent triggerRotateLeft;
     public SpatialInteractable triggerRotateLeft;
-    //public SpatialTriggerEvent triggerRotateBottom;
     public SpatialInteractable triggerRotateBottom;
+    public SpatialInteractable triggerRotateReset;
 
     // Start is called before the first frame update
     void Start()
     {
         triggerRotateLeft.onInteractEvent += RotateLeft;
         triggerRotateBottom.onInteractEvent += RotateBottom;
+        triggerRotateReset.onInteractEvent += RotateReset;
     }
 
     private void RotateLeft()
     {
         rotateObject.transform.Rotate(0, rotateObject.transform.rotation.y + 90, 0);
-        //rotateObject.transform.eulerAngles = new Vector3(
-        //    rotateObject.transform.eulerAngles.x,
-        //    rotateObject.transform.eulerAngles.y + 90,
-        //    rotateObject.transform.eulerAngles.z
-        //);
     }
 
     private void RotateBottom()
     {
         rotateObject.transform.Rotate(rotateObject.transform.rotation.x + 90, 0, 0);
-        //rotateObject.transform.eulerAngles = new Vector3(
-        //    rotateObject.transform.eulerAngles.x + 90,
-        //    rotateObject.transform.eulerAngles.y,
-        //    rotateObject.transform.eulerAngles.z
-        //);
+    }
+    private void RotateReset()
+    {
+        rotateObject.transform.localRotation = Quaternion.identity;
     }
 }
